df4c22716a84d2affc216645152f522ea2a4d5b4
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-11-04 20:49:49 -0500

    chore: add config files

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '.chemist',
+      'coverage',
+      'dist',
+      '.husky',
+      'lib',
+      'node_modules',
+      '.serverless',
+      'tmp',
+    ],
+  },
+  {
+    extends: [eslint.configs.recommended, ...tseslint.configs.strict],
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      'no-console': ['warn'],
+    },
+  }
+);
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  arrowParens: 'always',
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
